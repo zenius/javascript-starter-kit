b185382955221408a5ed2d28d7b1ec7f4c6b256f
zenius <zenius2050@gmail.com>
2019-07-26 16:08:50 +0530

    configuration for styles added in webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules'/, use: { loader: 'babel-loader'} }
+      { test: /\.js$/, exclude: /node_modules'/, use: { loader: 'babel-loader'} },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
     ]
   }
 };
