fdffe3ef1dfa980131cc3c11fc2cf04efafad138
zenius <zenius2050@gmail.com>
2019-07-31 19:06:51 +0530

    index.test.js file added

--- buildScripts/test-setup.js
+++ buildScripts/test-setup.js
@@ -1,7 +1,7 @@
 
 // Register babel to transpile before our tests run
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('babel-register');
-
 
 // Disable webpack features that Mocha don't understand
 // eslint-disable-next-line func-names
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,25 @@
 /* eslint-disable no-undef */
-const assert = require('assert');
+import assert from 'assert';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+const { JSDOM } = jsdom;
 
 describe('Array', () => {
   describe('#indexOf', () => {
     it('should return -1 when the value is not present', () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('index.html', () => {
+  describe('#helloWorld', () => {
+    it("should return 'Hello World'", () => {
+      const index = fs.readFileSync('./src/index.html', 'utf-8');
+      const dom = new JSDOM(index);
+      const h1 = dom.window.document.querySelector('h1').textContent;
+      assert.equal(h1, 'Hello World');
     });
   });
 });
