0bd79e60f05832fd3998ed1a24014fd3c175db4c
zenius <zenius2050@gmail.com>
2019-07-31 15:11:23 +0530

    eslint setup added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.css';
 
+// eslint-disable-next-line no-console
 console.log('Welcome to Webpack configuration');
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,18 +3,18 @@ import path from 'path';
 module.exports = {
   mode: 'development',
   entry: {
-    index: path.resolve(__dirname, 'src/index.js')
+    index: path.resolve(__dirname, 'src/index.js'),
   },
   devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules'/, use: { loader: 'babel-loader'} },
+      { test: /\.js$/, exclude: /node_modules'/, use: { loader: 'babel-loader' } },
       { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-    ]
-  }
+    ],
+  },
 };
